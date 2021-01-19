682a5521b9b31badad326ff5458a9046ece66588
Edmand9807 <ruslanalex98@icloud.com>
2021-01-19 12:04:33 +0300

    secondary

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed } = require('discord.js');
+const { readdirSync } = require('fs')
 
 const client = new Discord.Client();
 
-client.once('ready', () => {
+client.on('ready', () => {
     console.log('Ready!');
+    client.user.setActivity(`music | ${prefix}play`,  { type: "LISTENING" });
 });
 
 client.login(token);
@@ -51,27 +53,26 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).trim().split(' ');
     const command = args.shift().toLowerCase();
 
-    if (!message.guild) return;
     if (message.author.username === 'isymfc')
-    if (command === 'kick') {
-        const user = message.mentions.users.first();
-        if (user) {
-            const member = message.guild.member(user);
-            if (member) {
-                member.kick('kk')
-                    .then(() => {
-                        message.reply(`Successfully kicked ${user.tag}`);
-                    })
-                    .catch(err => {
-                        message.reply('I was unable to kick the member');
-                        console.error(err);
-                    });
+        if (command === 'kick') {
+            const user = message.mentions.users.first();
+            if (user) {
+                const member = message.guild.member(user);
+                if (member) {
+                    member.kick('kk')
+                        .then(() => {
+                            message.reply(`Successfully kicked ${user.username}`);
+                        })
+                        .catch(err => {
+                            message.reply(`I was unable to kick ${user}`);
+                            console.error(err);
+                        });
                 } else {
                     message.reply('You are not Ruslan')
                 }
             } else {
-            message.reply('You did not mention the user to kick');
+                message.reply('You did not mention the user to kick');
+            }
         }
-    }
 });
 
